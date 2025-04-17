d1097508936a4b3e90eb0cbac8ba755738a5dffa
kul-sudo <ulyanov.kirill@protonmail.com>
2025-04-17 23:39:58 +0300

    add split chart

--- src/book.rs
+++ src/book.rs
@@ -1,4 +1,4 @@
-use crate::histogram::save_histogram;
+use crate::histogram::save_histogram_split;
 use std::fs::read_to_string;
 
 const SEPARATOR_STRING: &str = "DMEUISFEDGNGEnyNGEGF*^ng679E67g9E^9nf7egwsn67f^G&EGN^EFN^G&FEG^&feGN^&g(F^6gfe7N^(GFBn9gN(^VE69gN^(GEng697efVG^97VGn6976n79VEFg6n79EYGFEHJEYUGgFEFEFEF][e]f[E}[EUHEG&*G]]))))"; // A string that can't occur in the given text
@@ -14,31 +14,17 @@ pub fn book() {
         contents = contents.replace("  ", " ");
     }
 
-    contents = contents.replace("“", r#"""#).replace("”", r#"""#);
-
-    for substr in [". ", r#"." "#, r#"?" "#, r#"!" "#] {
+    for substr in [". ", r#".” "#, r#"?” "#, r#"!” "#] {
         contents = contents.replace(substr, &(substr.to_string() + SEPARATOR_STRING));
     }
 
-    let mut statistics: [u128; 10] = [0; 10];
+    let mut lengths = Vec::new();
 
-    for sentence in contents.split(SEPARATOR_STRING).collect::<Vec<_>>() {
-        statistics[(sentence.len() - 1)
-            .to_string()
-            .chars()
-            .next()
-            .unwrap()
-            .to_string()
-            .parse::<usize>()
-            .unwrap()] += 1;
+    for sentence in contents.split(SEPARATOR_STRING) {
+        if !sentence.is_empty() {
+            lengths.push(sentence.len() as u128);
+        }
     }
 
-    let n = statistics.iter().sum::<u128>();
-
-    save_histogram(
-        10,
-        statistics.map(|x| x as f64 / n as f64).to_vec(),
-        "book.png",
-    )
-    .unwrap();
+    save_histogram_split(lengths, "book.png").unwrap();
 }
--- src/histogram.rs
+++ src/histogram.rs
@@ -1,3 +1,4 @@
+use crate::int_to_base;
 use plotters::prelude::*;
 
 pub fn save_histogram(
@@ -53,6 +54,85 @@ pub fn save_histogram(
             .data(benford_predictions)
             .margin(20),
     )?;
+
+    Ok(())
+}
+
+pub fn save_histogram_split(
+    data: Vec<u128>,
+    output_file: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new(output_file, (1280, 960))
+        .into_drawing_area()
+        .titled(
+            "Red: Reality / Black: Benford's law prediction",
+            ("sans-serif", 15.0),
+        )
+        .unwrap();
+    root.fill(&WHITE)?;
+
+    let areas = root.split_evenly((4, 3));
+
+    for (i, area) in areas.iter().enumerate() {
+        let base = i as u128 + 3;
+        let benford_predictions = (1..base)
+            .map(|d| {
+                (
+                    d,
+                    ((((d + 1) as f64).log(base as f64) - (d as f64).log(base as f64)) * 100.0)
+                        .round() as u128,
+                )
+            })
+            .collect::<Vec<_>>();
+        let benford_max = benford_predictions.iter().map(|(_, x)| *x).max().unwrap();
+
+        let mut statistics = vec![0; base as usize];
+        for x in &data {
+            statistics[*int_to_base(*x, base).last().unwrap() as usize] += 1;
+        }
+
+        let statistics_percent = statistics
+            .iter()
+            .map(|x| {
+                ((*x as f64 / statistics[1..].iter().sum::<u128>() as f64) * 100.0).round() as u128
+            })
+            .collect::<Vec<_>>();
+
+        let data_max = statistics_percent.iter().max().unwrap();
+        let max = benford_max.max(*data_max);
+
+        let mut chart = ChartBuilder::on(area)
+            .x_label_area_size(35)
+            .y_label_area_size(40)
+            .margin(5)
+            .caption(base.to_string(), ("sans-serif", 15.0))
+            .build_cartesian_2d((1..base - 1).into_segmented(), 0u128..max + 5)?;
+
+        chart
+            .configure_mesh()
+            .disable_x_mesh()
+            .bold_line_style(WHITE.mix(0.3))
+            .y_desc("%")
+            .x_desc("First digit")
+            .axis_desc_style(("sans-serif", 12))
+            .draw()?;
+
+        chart.draw_series(
+            Histogram::vertical(&chart).style(RED.filled()).data(
+                statistics_percent
+                    .iter()
+                    .enumerate()
+                    .map(|(index, x)| (index as u128, *x)),
+            ),
+        )?;
+
+        chart.draw_series(
+            Histogram::vertical(&chart)
+                .style(BLACK.filled())
+                .data(benford_predictions.iter().map(|&(index, x)| (index, x)))
+                .margin(12),
+        )?;
+    }
 
     Ok(())
 }
